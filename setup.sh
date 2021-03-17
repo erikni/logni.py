145f5624f71319c6dd791d2eb952a26ec0e203ee
Erik Brozek <erik@brozek.name>
2021-03-17 14:45:13 +0100

    Fixed:  syntax error near unexpected token `|'

--- setup.sh
+++ setup.sh
@@ -84,7 +84,7 @@ sudo mkdir -p ${INST_DIR}/logni
 echo "done."
 
 echo -n "${GIT_REPO}: * script install ... "
-for pyfile in `find ${TMP_DIR}/logni | grep "\.py" | grep -v '__pycache__' | grep -v "\.pyc" | grep -v "pylintrc" | grep -v "test/"` | grep -v "\.pytest"; do 
+for pyfile in `find ${TMP_DIR}/logni | grep "\.py" | grep -v '__pycache__' | grep -v "\.pyc" | grep -v "pylintrc" | grep -v "test/" | grep -v "\.pytest"`; do 
 	sudo cp ${pyfile} ${INST_DIR}/logni/.; 
 done;
 echo "done."
